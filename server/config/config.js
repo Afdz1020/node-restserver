24aaa90d5970f3bc0d95525b0ed44b8fe0260792
Andrés Felipe Díaz Zapata <adiaz@pratech.local>
2019-12-04 12:47:38 -0500

    deploy heroku

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ======================
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================
+// Entorno
+// ======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================
+// Base de datos
+// ======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://diaz:diaz123456@ds135335.mlab.com:35335/diaz-test';
+} else {
+  urlDB =
+    'mongodb+srv://andresdiaz:yMIede4xjrGu7wZU@cluster0-bu0vr.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,24 @@
-require("./config/config");
-const express = require("express");
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.get("/usuario", (req, res) => {
-  res.json("getUsuarios");
-});
+app.use(require('./routes/usuario'));
 
-app.post("/usuario", (req, res) => {
-  let body = req.body;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario"
-    });
-  } else {
-    res.json({
-      persona: body
-    });
+mongoose.connect(
+  `${process.env.URLDB}`,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
   }
-});
-
-app.put("/usuario/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({ id });
-});
-
-app.delete("/usuario", (req, res) => {
-  res.json("delete Usuarios");
-});
+);
 
 app.listen(process.env.PORT, () => {
-  console.log("Escuchando puerto : ", process.env.PORT);
+  console.log('Escuchando puerto : ', process.env.PORT);
 });
