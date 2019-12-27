876f9524380221509cfe868113738709a4ee0186
Andrés Felipe Díaz Zapata <adiaz@pratech.local>
2019-12-27 12:58:37 -0500

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -34,7 +34,28 @@ let verificarAdmin_Role = (req, res, next) => {
       .json({ of: false, err: { message: 'El usuario no es administrador' } });
   }
 };
+
+//=======================
+// Verificar token para imagen
+//=====================
+
+let verificarTokenImg = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ of: false, err: { message: 'Token no valido' } });
+    }
+
+    req.usuario = decoded.usuario;
+    next();
+  });
+};
+
 module.exports = {
   verificarToken,
-  verificarAdmin_Role
+  verificarAdmin_Role,
+  verificarTokenImg
 };
